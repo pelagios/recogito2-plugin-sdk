f83f81b1d8edc77dbf0493d25e7ee2337b91e631
Rainer Simon <rainer.simon@ait.ac.at>
2019-04-02 13:24:08 +0200

    Attempting to bring findFile method into SDK as utility function

--- src/main/java/org/pelagios/recogito/sdk/PluginEnvironment.java
+++ src/main/java/org/pelagios/recogito/sdk/PluginEnvironment.java
@@ -1,6 +1,9 @@
 package org.pelagios.recogito.sdk;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map; 
 import java.util.HashMap;
 
@@ -32,6 +35,45 @@ public class PluginEnvironment {
   /** Plugin config file contents (if any) **/
   public Map<String, String> getPluginConfig() {
     return config;
+  }
+
+  /** Recursive method that does the searching for .findFile **/
+  private File findRecursive(String filename, File folder) {
+    File[] files = folder.listFiles();
+
+    File match = null; // I hate java
+    for (File f : files) {
+      if (f.getName() == filename && f.isFile())
+        match = f;
+    }
+
+    if (match == null) {
+      List<File> subfolders = new ArrayList<File>();
+      for (File f : files) {
+        if (f.isDirectory())
+          subfolders.add(f);
+      }
+
+      // Recurse for all subfolders
+      for (File subfolder: subfolders) {
+        File maybeMatch = findRecursive(filename, subfolder);
+        if (maybeMatch != null) 
+          return maybeMatch;        
+      }
+
+      return null;
+    } else {
+      return match;
+    }
+  }
+
+  /** Finds the given file in the environment's plugin folder **/
+  public String findFile(String filename) {
+    File f = findRecursive(filename, this.pluginsFolder);
+    if (f == null) 
+      return null;
+    else
+      return f.getAbsolutePath();
   }
 
 }
