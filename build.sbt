28f3a3c1758e3aef34a6632bbb61d94665507e3a
Rainer Simon <rainer.simon@ait.ac.at>
2019-09-16 11:41:33 +0200

    Allowing custom entity types + multiple labels

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "recogito-plugin-sdk"
 
 organization := "org.pelagios"
 
-version := "1.0"
+version := "2.0-RC"
 
 scalaVersion := "2.11.11"
 
--- src/main/java/org/pelagios/recogito/sdk/ner/EntityType.java
+++ src/main/java/org/pelagios/recogito/sdk/ner/EntityType.java
@@ -1,8 +1,39 @@
 package org.pelagios.recogito.sdk.ner;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * To be extended in the future, potentially.
  *
  * @author Rainer Simon <rainer.simon@ait.ac.at>
  */
-public enum EntityType { LOCATION, PERSON, DATE };
+public class EntityType { 
+
+  // Just some default helpers
+  public static EntityType LOCATION = new EntityType("LOCATION");
+  public static EntityType PERSON   = new EntityType("PERSON");
+  public static EntityType EVENT    = new EntityType("EVENT");
+
+  private final String name;
+
+  private List<String> labels = new ArrayList();
+
+  public EntityType(String name) {
+    this.name = name;
+  }
+
+  public void addLabel(String label) {
+    this.labels.add(label);
+  }
+
+  public List<String> getLabels() {
+    return this.labels;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
+
+}
